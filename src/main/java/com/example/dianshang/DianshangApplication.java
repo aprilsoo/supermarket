f3ea29fde2b034c04f3da8c733c21b0178c0ffe3
Chends123 <Chends123456@gmail.com>
2023-05-20 09:10:52 +0800

    项目创建

---
+++ src/main/java/com/example/dianshang/DianshangApplication.java
@@ -0,0 +1,13 @@
+package com.example.dianshang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DianshangApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DianshangApplication.class, args);
+    }
+
+}
